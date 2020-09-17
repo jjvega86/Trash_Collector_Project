cfd84f560e51aa16a4e799b6d8df9aeb2385a480
jjvega86 <jj.vega86@gmail.com>
2020-09-17 16:11:23 -0400

    Migration added for Employee model properties

---
+++ Trash_Collector/Data/Migrations/20200917201050_AddEmployeeProperties.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trash_Collector.Data.Migrations
+{
+    public partial class AddEmployeeProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5db0850f-636d-4d52-930f-cf163ad6f0d4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "88210483-388e-4ab2-91a9-b8d765e2bfa6");
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    ZipCodeAssignment = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "f6fc54a6-a9fe-4f03-b20d-f37c16d04c50", "ea1365cf-1a12-4c34-bd06-0747d36a2839", "Customer", "CUSTOMER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "e96e4321-9198-466e-bbd3-c6ad8c04e75b", "997697eb-46d2-43b1-8112-07c83d26f6fb", "Employee", "EMPLOYEE" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e96e4321-9198-466e-bbd3-c6ad8c04e75b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f6fc54a6-a9fe-4f03-b20d-f37c16d04c50");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "5db0850f-636d-4d52-930f-cf163ad6f0d4", "5b560919-f31a-4489-a826-14965b91577d", "Customer", "CUSTOMER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "88210483-388e-4ab2-91a9-b8d765e2bfa6", "5e3af1b9-b14e-4dbd-9662-7615de6f2e11", "Employee", "EMPLOYEE" });
+        }
+    }
+}
--- Trash_Collector/Models/Employee.cs
+++ Trash_Collector/Models/Employee.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,11 @@ namespace Trash_Collector.Models
     {
         [Key]
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ZipCodeAssignment { get; set; }
 
+        [NotMapped]
+        public IEnumerable<Customer> Customers { get; set; }
     }
 }
