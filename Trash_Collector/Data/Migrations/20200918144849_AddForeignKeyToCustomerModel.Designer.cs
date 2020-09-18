313e7867953c808f1d88650d0c22ebdba8930c76
jjvega86 <jj.vega86@gmail.com>
2020-09-18 10:50:00 -0400

    Added migration to reflect updated Customer model

--- Trash_Collector/Data/Migrations/20200918144849_AddForeignKeyToCustomerModel.Designer.cs
+++ Trash_Collector/Data/Migrations/20200918144849_AddForeignKeyToCustomerModel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Trash_Collector.Data;
 
 namespace Trash_Collector.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200918144849_AddForeignKeyToCustomerModel")]
+    partial class AddForeignKeyToCustomerModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Trash_Collector/Data/Migrations/20200918144849_AddForeignKeyToCustomerModel.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trash_Collector.Data.Migrations
+{
+    public partial class AddForeignKeyToCustomerModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e96e4321-9198-466e-bbd3-c6ad8c04e75b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f6fc54a6-a9fe-4f03-b20d-f37c16d04c50");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityUserId",
+                table: "Customers",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "bcc78108-3b55-429f-bc98-e3db51d74e39", "77cd8d36-dcfd-4492-b8c6-40f32e4ecef8", "Customer", "CUSTOMER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "07c19a8a-e048-49f5-aa48-88efb88e817c", "27bd5daa-146a-4bca-bea1-35fba963bd2d", "Employee", "EMPLOYEE" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_IdentityUserId",
+                table: "Customers",
+                column: "IdentityUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_AspNetUsers_IdentityUserId",
+                table: "Customers",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_AspNetUsers_IdentityUserId",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_IdentityUserId",
+                table: "Customers");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "07c19a8a-e048-49f5-aa48-88efb88e817c");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "bcc78108-3b55-429f-bc98-e3db51d74e39");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityUserId",
+                table: "Customers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "f6fc54a6-a9fe-4f03-b20d-f37c16d04c50", "ea1365cf-1a12-4c34-bd06-0747d36a2839", "Customer", "CUSTOMER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "e96e4321-9198-466e-bbd3-c6ad8c04e75b", "997697eb-46d2-43b1-8112-07c83d26f6fb", "Employee", "EMPLOYEE" });
+        }
+    }
+}
